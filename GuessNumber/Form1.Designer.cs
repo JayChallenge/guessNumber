b351563522507997ea287d30c62a12a363468f27
JayChallenge <xiaotianxing1@yeah.net>
2016-06-12 14:48:25 +0800

    The original codes.
    C# codes.

---
+++ GuessNumber/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GuessNumber
+{
+    partial class FormGuessNumber
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(51, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(105, 49);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "开始生成数字";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(12, 81);
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(105, 21);
+            this.textBoxGuess.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Enabled = false;
+            this.buttonOK.Location = new System.Drawing.Point(135, 79);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(51, 22);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "确定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(49, 137);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 12);
+            this.labelResult.TabIndex = 3;
+            // 
+            // FormGuessNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 191);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxGuess);
+            this.Controls.Add(this.buttonStart);
+            this.MaximizeBox = false;
+            this.Name = "FormGuessNumber";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "猜数字游戏";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxGuess;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ GuessNumber/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuessNumber
+{
+    public partial class FormGuessNumber : Form
+    {
+        private String result=null;
+        private int guessCount = 0;
+        
+        public FormGuessNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            List<Char> shuzi =new List<Char>();
+            shuzi.Add('0');
+            shuzi.Add('1');
+            shuzi.Add('2');
+            shuzi.Add('3');
+            shuzi.Add('4');
+            shuzi.Add('5');
+            shuzi.Add('6');
+            shuzi.Add('7');
+            shuzi.Add('8');
+            shuzi.Add('9');
+            
+            Random random=new Random();
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<4;i++){
+                int re=random.Next(shuzi.Count);
+                sb.Append(shuzi[re]);
+                shuzi.RemoveAt(re);
+            }
+            result = sb.ToString();
+            guessCount = 0;
+            textBoxGuess.Text = "";
+            buttonStart.Enabled = false;
+            buttonOK.Enabled = true;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            int acount = 0;
+            int bcount = 0;
+            String guess = textBoxGuess.Text;
+            if (guess.Length != 4)
+            {
+                MessageBox.Show("输入数字个数错误");
+            }
+            else
+            {
+                Dictionary<char, int> dic = new Dictionary<char, int>();
+                int k=0;
+                for (k = 0; k < guess.Length; k++)
+                {
+                    if (dic.ContainsKey(guess[k]))
+                    {
+                        dic[guess[k]]+=1;
+                        break;
+                    }
+                    else 
+                    {
+                        dic.Add(guess[k],1);
+                    }
+                }
+                if (k < guess.Length)
+                {
+                    MessageBox.Show("您输入了重复的数字！");
+                }
+                else
+                { 
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        for (int j = 0; j < guess.Length; j++)
+                        {
+                            if (guess[j] == result[i])
+                            {
+                                bcount++;
+                                if (j == i)
+                                {
+                                    acount++;
+                                }
+                            }
+                        }
+                    }
+                    bcount -= acount;
+                    labelResult.Text = "您猜的结果是:" + acount + "A" + bcount + "B";
+                    guessCount++;
+                    if (acount == 4)
+                    {
+                        labelResult.Text += "\n您猜对了，恭喜！答案是:" + result + "\n共猜了：" + guessCount + "次";
+                        buttonOK.Enabled = false;
+                        buttonStart.Enabled = true;
+                    }
+                }
+            }
+            
+        }
+    }
+}
